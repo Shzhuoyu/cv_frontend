a895d6242b1d633771e04200fcab31a2804925b4
shzhuoyu <971993723@qq.com>
2020-07-01 15:51:55 +0800

    接口的更改和person表的的填写

--- components/personManagement/personDetail.vue
+++ components/personManagement/personDetail.vue
@@ -1,10 +1,73 @@
 <template>
+    <el-dialog
+            :title="title"
+            :visible.sync="centerDetailVisible"
+            width="60%"
+            center
+    >
+        <el-form :model="form">
+            <el-form-item label="活动名称" label-width="600px">
+                <el-input v-model="form.name" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="活动区域" label-width="70%">
+                <el-select v-if="false" v-model="form.region" placeholder="请选择活动区域">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+            </el-form-item>
+        </el-form>
+
+
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="setHide">取 消</el-button>
+    <el-button type="primary" @click="setHide">确 定</el-button>
+  </span>
+
+    </el-dialog>
 
 </template>
 
 <script>
     export default {
-        name: "personDetail"
+        name: "personDetail",
+        data(){
+            return{
+                title:"",
+                type:"",
+                typeName:"",
+                edit:false,
+                centerDetailVisible: false,
+                form:{name:''}
+
+
+
+            }
+        },
+        methods:{
+            setVisible(){
+                this.centerDetailVisible = true
+            },
+            setHide(){
+                this.centerDetailVisible = false
+            },
+            setTypeEdit(data){ //设置 种类及
+                console.log(data)
+                this.type = data.type;
+                this.edit = data.edit;
+
+                if (this.type == '0'){this.typeName= '老人' }
+                else if (this.type == '1'){this.typeName = '员工'}
+                else {this.typeName = "义工"}
+
+                console.log(this.typeName)
+
+                if (this.edit == true){
+                    this.title = this.typeName+"编辑"
+                }else {
+                    this.title = this.typeName+"详情"
+                }
+            }
+        }
     }
 </script>
 
--- components/personManagement/personList.vue
+++ components/personManagement/personList.vue
@@ -1,10 +1,122 @@
 <template>
+    <div>
+    <el-table
+            :data="tableData"
+            :stripe=true
+            style="width: 90%">
+
+        <el-table-column
+                label="ID"
+                width="80"
+                prop="id"
+        ></el-table-column>
+        <el-table-column
+                label="头像"
+                width="180">
+            <template slot-scope="scope">
+                <span style="margin-left: 10px">{{ scope.row.birthday}}</span>
+            </template>
+        </el-table-column>
+
+        <el-table-column
+                label="姓名"
+                width="180"
+                prop="username"
+        ></el-table-column>
+
+        <el-table-column
+                label="性别"
+                width="80"
+                prop="gender"
+        ></el-table-column>
+
+        <el-table-column
+                label="电话"
+                width="180"
+                prop="phone"
+        ></el-table-column>
+
+        <el-table-column
+                label="生日"
+                width="180">
+            <template slot-scope="scope">
+                <i class="el-icon-time"></i>
+                <span style="margin-left: 10px">{{ scope.row.birthday}}</span>
+            </template>
+        </el-table-column>
+
+        <el-table-column label="操作">
+            <template slot-scope="scope">
+                <el-button
+                        size="mini"
+                        @click="handleDetail(scope.$index, scope.row)">详情</el-button>
+                <el-button
+                        size="mini"
+                        @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                <el-button
+                        size="mini"
+                        type="danger"
+                        @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+        <person-detail ref="detail"></person-detail>
+
+
+
+    </div>
 
 </template>
 
 <script>
+    import PersonDetail from "./personDetail";
     export default {
-        name: "personList"
+        name: "personList",
+        components: {PersonDetail},
+        data(){
+            return{
+                type:"0", //判断是老人还是员工还是义工
+
+                tableData: [{
+
+                }]
+            }
+        },
+        methods:{
+            handleDetail(){
+
+                let data = {
+                    type: this.type,
+                    edit: false
+                }
+                this.$refs.detail.setTypeEdit(data)
+                this.$refs.detail.setVisible()
+
+
+            },
+            handleEdit(){
+
+
+                let data = {
+                    type: this.type,
+                    edit: true   //可编辑
+                }
+                this.$refs.detail.setTypeEdit(data)
+                this.$refs.detail.setVisible()
+
+            },
+            handleDelete(){
+
+            },
+            setData(data){
+                this.tableData = data
+                console.log(this.tableData)
+            },
+            setType(type){
+                this.type = type
+            }
+
+        }
     }
 </script>
 
