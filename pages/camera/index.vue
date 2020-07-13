ce851233bc76f49e6dc4aed959d6cbe054a74883
shzhuoyu <971993723@qq.com>
2020-07-13 11:52:52 +0800

    人数

--- pages/camera/index.vue
+++ pages/camera/index.vue
@@ -49,58 +49,7 @@
                 </el-row>
             </div>
             <div style="margin-top: 30px"></div>
-            <div>
-                <el-row style="margin-left:80px; width: 1200px" type="flex" justify="space-between">
-                    <el-col :span="15" class="card">
-                        <el-card shadow="hover" :body-style="{padding: '0px'}">
-                            <div class="grid-content grid-con-1">
-                                <i class="el-icon-user grid-con-icon"></i>
-                                <div class="grid-cont-right">
-                                    <div class="grid-num">{{elderNum}}</div>
-                                    <span class="subtext">{{currentTime}}</span>
-                                    <div class="maintext">老人数量</div>
-                                </div>
-                            </div>
-                        </el-card>
-                    </el-col>
-                    <el-col :span="15" class="card">
-                        <el-card shadow="hover" :body-style="{padding: '0px'}">
-                            <div class="grid-content grid-con-2">
-                                <i class="el-icon-user grid-con-icon"></i>
-                                <div class="grid-cont-right">
-                                    <div class="grid-num">{{staffNum}}</div>
-                                    <span class="subtext">{{currentTime}}</span>
-                                    <div class="maintext">工作人员数量</div>
-                                </div>
-                            </div>
-                        </el-card>
-                    </el-col>
-                    <el-col :span="15" class="card">
-                        <el-card shadow="hover" :body-style="{padding: '0px'}">
-                            <div class="grid-content grid-con-4">
-                                <i class="el-icon-user grid-con-icon"></i>
-                                <div class="grid-cont-right">
-                                    <div class="grid-num">{{volunteerNum}}</div>
-                                    <span class="subtext">{{currentTime}}</span>
-                                    <div class="maintext">义工数量</div>
-                                </div>
-                            </div>
-                        </el-card>
-                    </el-col>
-                    <el-col :span="15" class="card">
-                        <el-card shadow="hover" :body-style="{padding: '0px'}">
-                            <div class="grid-content grid-con-3">
-                                <i class="el-icon-user grid-con-icon"></i>
-                                <div class="grid-cont-right">
-                                    <div class="grid-num">{{strangerNum}}</div>
-                                    <span class="subtext">{{currentTime}}</span>
-                                    <div class="maintext">陌生人数量</div>
-                                </div>
-                            </div>
-                        </el-card>
-                    </el-col>
-                </el-row>
-            </div>
+            <total-num></total-num>
         </div>
     </div>
 </template>
@@ -108,10 +57,11 @@
 <script>
     import CliTitle from "../../components/base/cliTitle";
     import CliMenu from "../../components/base/cliMenu";
+    import TotalNum from "../../components/camera/totalNum";
 
     export default {
         name: "index",
-        components: {CliTitle, CliMenu},
+        components: {TotalNum, CliTitle, CliMenu},
         data() {
             return {
                 mainSrc: "rtmp://39.97.124.237:1984/wodelive/",
@@ -123,12 +73,6 @@
                 Src1Name: "走廊",
                 Src2Name: "院子",
                 Src3Name: "桌子",
-                timer: "",//定义一个定时器的变量
-                currentTime: new Date(), // 获取当前时间
-                elderNum: 0,
-                staffNum:0,
-                volunteerNum: 0,
-                strangerNum:0,
                 isRouterAlive: true,
             }
         },
@@ -166,28 +110,7 @@
                 this.reload();
             }
         },
-        created() {
-            var _this = this; //声明一个变量指向Vue实例this，保证作用域一致
-            this.timer = setInterval(function () {
-                _this.currentTime = //修改数据date
-                    new Date().getFullYear() +
-                    "-" +
-                    (new Date().getMonth() + 1) +
-                    "-" +
-                    new Date().getDate() +
-                    " " +
-                    new Date().getHours() +
-                    ":" +
-                    new Date().getMinutes() +
-                    ":" +
-                    new Date().getSeconds();
-            }, 1000);
-        },
-        beforeDestroy() {
-            if (this.timer) {
-                clearInterval(this.timer); // 在Vue实例销毁前，清除我们的定时器
-            }
-        }
+
     }
 </script>
 
@@ -201,79 +124,10 @@
         margin-bottom: 10px;
     }
 
-    .maintext {
-        font-size: 16px;
-        text-align: center;
-    }
-
     .subtext {
         font-size: 14px;
         text-align: center;
     }
 
-    .card {
-        text-align: center;
-        margin-top: 20px;
-        width: 250px;
-    }
-
-    .grid-content {
-        display: flex;
-        align-items: center;
-        height: 100px;
-    }
-
-    .grid-cont-right {
-        flex: 1;
-        text-align: center;
-        font-size: 14px;
-        color: #999;
-    }
-
-    .grid-num {
-        font-size: 30px;
-        font-weight: bold;
-    }
-
-    .grid-con-icon {
-        font-size: 50px;
-        width: 100px;
-        height: 100px;
-        text-align: center;
-        line-height: 100px;
-        color: #fff;
-    }
-
-    .grid-con-1 .grid-con-icon {
-        background: rgb(45, 140, 240);
-    }
-
-    .grid-con-1 .grid-num {
-        color: rgb(45, 140, 240);
-    }
-
-    .grid-con-2 .grid-con-icon {
-        background: rgb(100, 213, 114);
-    }
-
-    .grid-con-2 .grid-num {
-        color: rgb(45, 140, 240);
-    }
-
-    .grid-con-3 .grid-con-icon {
-        background: rgb(242, 94, 67);
-    }
-
-    .grid-con-3 .grid-num {
-        color: rgb(242, 94, 67);
-    }
-
-    .grid-con-4 .grid-con-icon {
-        background: rgb(242, 229, 87);
-    }
-
-    .grid-con-4 .grid-num {
-        color: rgb(45, 140, 240);
-    }
 
 </style>
